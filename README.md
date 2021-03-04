93c043839bad99a3d1dcb1762d76201c5dde5b20
Christopher Arndt <chris@chrisarndt.de>
2021-03-04 18:05:52 +0100

    Initial commit
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ README.md
@@ -0,0 +1,14 @@
+# nc_share.py
+
+A simple Python script to upload an image to my Nextcloud server and create a
+sharing link, which is then placed in the clipboard
+
+Based on [pyocclient], in a form slightly altered by me in the file
+`nextcloud.py`.
+
+Released under the MIT License.
+
+See the [LICENSE](./LICENSE) file for more infomration
+
+
+[pyocclient]: https://github.com/owncloud/pyocclient/
---
+++ nc_share.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""Upload a file to a Nextcloud instance then share with link."""
+
+import argparse
+from os.path import basename
+from posixpath import join as pjoin
+
+import keyring
+import pyperclip
+
+from . import nextcloud
+
+NEXTCLOUD_URL = "https://0x20.eu/nc/"
+DEST_DIR = pjoin("xchange", "img")
+
+
+def main(args=None):
+    ap = argparse.ArgumentParser(usage=__doc__.splitlines()[0])
+    ap.add_argument(
+        "-u", "--url", default=NEXTCLOUD_URL, help="Nextcloud instance URL (default: %(default)s)"
+    )
+    ap.add_argument(
+        "-d",
+        "--destdir",
+        default=DEST_DIR,
+        help="Remote destination directory (default: %(default)s).",
+    )
+    ap.add_argument("-U", "--user", help="Nextcloud user name (default: local username)")
+    ap.add_argument("-P", "--password", help="Nextcloud password (default: use keyring)")
+    ap.add_argument(
+        "-p", "--preview", action="store_true", help="Add '/preview' suffix to share link"
+    )
+    ap.add_argument("-v", "--verbose", action="store_true", help="Be more verbose.")
+    ap.add_argument("srcfile", help="Local source file.")
+    args = ap.parse_args(args)
+
+    oc = nextcloud.Client(args.url, debug=args.verbose)
+
+    user = args.user if args.user else getpass.getuser()
+
+    password = args.password if args.password else keyring.get_password(args.url, user)
+
+    if not password:
+        return "No password found in keyring for '{}' on '{}'.".format(user, args.url)
+
+    oc.login(args.user, password)
+
+    bn = basename(args.srcfile)
+    dst = pjoin(args.destdir, bn)
+    oc.put_file(dst, args.srcfile)
+
+    link_info = oc.share_file_with_link(dst)
+    link_url = link_info.get_link()
+
+    if args.preview:
+        link_url += "/preview"
+
+    if args.verbose:
+        print("Share link URL: {}".format(link_url))
+
+    pyperclip.copy(link_url)
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(main() or 0)
